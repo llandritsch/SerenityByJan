197110b281e7f098220ffc76a45df4020a009c34
Jordyn Barbiaux <jfisher9@madisoncollege.edu>
2021-04-01 11:15:08 -0500

    Updating getAllCharacters method to use try/catch instead of throwing exception

--- src/main/java/edu/matc/api/CharacterService.java
+++ src/main/java/edu/matc/api/CharacterService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.matc.entity.Character;
 import edu.matc.persistence.CharacterDao;
+import lombok.extern.log4j.Log4j2;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Log4j2
 @Path("/characters")
 public class CharacterService {
 
@@ -18,7 +20,7 @@ public class CharacterService {
 
     @GET
     @Produces("application/json")
-    public Response getCharacters() throws JsonProcessingException {
+    public Response getAllCharacters() {
         CharacterDao dao = new CharacterDao();
         List<Character> characters = dao.getAllCharacters();
         ObjectMapper mapper = new ObjectMapper();
@@ -26,11 +28,22 @@ public class CharacterService {
         String output = "";
 
         for (Character character : characters) {
-            String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(character);
-            output += jsonString;
+            try {
+                String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(character);
+                output += jsonString;
+            } catch (JsonProcessingException e){
+                log.error(e);
+            }
+
         }
 
         return Response.status(200).entity(output).build();
-
     }
+//
+//    public Response getCharacterByName() {
+//
+//        return Response.status(200).entity(output).build();
+//    }
+
+
 }
