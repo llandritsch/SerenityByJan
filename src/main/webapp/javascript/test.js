7bf1cdea2950cf234bf9eb9110f2a48c37e03091
Lisa Andritsch <lisa3718@gmail.com>
2021-04-08 12:42:53 -0500

    m

--- src/main/webapp/javascript/test.js
+++ src/main/webapp/javascript/test.js
@@ -4,10 +4,4 @@ const init = () => {
     document.body.appendChild(p);
 }
 
-const requestGET = async () => {
-    const response = await fetch('http://localhost:8080/SerenityByJan_war/characters/');
-    console.log(response);
-    document.getElementById('requestOutput').innerHTML = response;
-}
-
 window.onload = init;
